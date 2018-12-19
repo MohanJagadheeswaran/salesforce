99768ecf4fce6e4156d6794ff73bdcd40b24ae93
agoda <jagadheeswaran.mohan@gmail.com>
2018-12-19 17:00:54 +0530

    Initial Commit

--- blockcontent.js
+++ blockcontent.js
@@ -2,6 +2,7 @@ var sdk = new window.sfdc.BlockSDK();
 
 var setContentToBuilder = function() {
     var a = document.getElementById('assetName').value;
+    console.log(a);
     sdk.setContent('<h1>'+ assetName+ '</h1>')
 }
 
